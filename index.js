a68336cc18cd48e21510304b4395c3e28ed088ab
bobbyhadz <opmftw@gmail.com>
2024-03-05 06:24:36 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log('bobbyhadz.com');
+
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', function onClick(event) {
+  // 👇️ change background color
+  document.body.style.backgroundColor = 'salmon';
+
+  // 👇️ optionally change text color
+  // document.body.style.color = 'white';
+});
